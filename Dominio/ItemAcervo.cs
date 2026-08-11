57eb8f65160f2452371729959cc499304c7d1f06
VItorFer.dev <vitorfer3537@gmail.com>
2026-08-11 19:50:55 -0300

    init: inicialização do projeto, criação da primeira classe em C#, classe abstrata inicial do projeto

---
+++ Dominio/ItemAcervo.cs
@@ -0,0 +1,14 @@
+namespace Biblioteca.Dominio;
+
+public abstract class ItemAcervo
+{
+    public string Titulo { get; set; } = string.Empty;
+    public string Autor { get; set; } = string.Empty;
+    public bool Disponibilidade { get; private set; } = true;
+    public abstract int PrazoDevolucao { get; set; }
+    public abstract decimal MultaDiaAtrasado { get; set; }
+    public decimal CalcularMulta(int diasAtrasados)
+    {
+        return diasAtrasados >= 0 ? diasAtrasados * MultaDiaAtrasado : 0;
+    }
+}
