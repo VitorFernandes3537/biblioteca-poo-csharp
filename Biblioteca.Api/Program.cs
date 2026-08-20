f7f642c29f8ef07743ab802e7a621f9c408d5437
VItorFer.dev <vitorfer3537@gmail.com>
2026-08-20 15:39:11 -0300

    feat: busca de item por id com 404 quando o item nao existe

--- Biblioteca.Api/Program.cs
+++ Biblioteca.Api/Program.cs
@@ -1,14 +1,25 @@
 using Biblioteca.Api;
-using Microsoft.AspNetCore.Authentication;
+using Biblioteca.Dominio;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
+// Seed feito FORA de Endpoints, fazendo com que ele sobreviva a requisicições
+// Pois todo endpoint que cria algo, esse algo morre assim que ele retornar
 var acervo = new Acervo();
 AcervoSeed.Popular(acervo);
 
 app.MapGet("/", () => Results.Redirect("/itens"));
 
 app.MapGet("/itens", () => acervo.Itens);
+
+app.MapGet("/itens/{id:int}", (int id) =>
+{
+    var itemEncontrado = acervo.BuscarPorId(id);
+    if (itemEncontrado is null)
+    {
+        return Results.NotFound(new { erro = $"O Item {id} não foi encontrado!" });
+    }
+    return Results.Ok(itemEncontrado);
+});
 
 app.Run();
